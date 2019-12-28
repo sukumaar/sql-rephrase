fd4bb95461e1fc7f3281d605e6f6a33dd2113e4c
sukumaar <sukumaar.neo@gmail.com>
2019-12-28 23:58:49 +0530

    initial commit

---
+++ src/main/java/org/apache/sqlrephrase/OracleCreateStatementConverter.java
@@ -0,0 +1,13 @@
+package org.apache.sqlrephrase;
+
+/**
+ * @author sukumaar
+ *
+ */
+public class OracleCreateStatementConverter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/apache/sqlrephrase/OracleCreateStatementConverterTest.java
@@ -0,0 +1,21 @@
+package org.apache.sqlrephrase;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple OracleCreateStatementConverter.
+ */
+public class OracleCreateStatementConverterTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+}
